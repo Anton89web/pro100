ad62ee1a50ecec4ab386b627493a6a45a509f1f3
“Anton <fenix-live@mail.com>
2024-07-06 18:29:02 +0300

    Добавил картинки и сделал header

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,16 @@
 import '../scss/index.scss'
 
-function test () {
-    console.log('test')
+function showMobileMenu () {
+    const menu = document.querySelector('.header__link-wrapper');
+    const icons = document.querySelectorAll('.header__mobile-icon');
+    const iconsWrapper = document.querySelector('.header__mobile');
+
+    icons.forEach(icon => {
+        icon.addEventListener('click', e => {
+            menu.classList.toggle('show')
+            iconsWrapper.classList.toggle('hidden')
+        } )
+    })
 }
 
-test()
+showMobileMenu ();
