e5ed47cc5245c93e4970b96e857192898b387848
Anton <fenix-live@mail.ru>
2024-07-16 11:58:15 +0300

    Убрал минимизацию css

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -43,6 +43,7 @@ export default {
                                         },
                                     ],
                                 ],
+                                minimize: false
                             },
                         },
                     },
@@ -50,6 +51,7 @@ export default {
                         loader: "sass-loader",
                         options: {
                             sourceMap: true,
+                            sassOptions: { minimum: false,outputStyle: 'expanded' }
                         },
                     },
                 ]
